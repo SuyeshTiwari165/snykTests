ebe15f335141ac0308f760e73f8cdb5f0a48d352
bhavesh.gaba@webaccessglobal.com <Bhavesh@123>
2021-08-03 18:24:32 +0530

    Navigation changes and Tooltip changes

--- src/containers/RiskAssessment/Windows_Network/Windows_Network.tsx
+++ src/containers/RiskAssessment/Windows_Network/Windows_Network.tsx
@@ -383,12 +383,20 @@ export const Windows_Network: React.FC = (props: any) => {
       history.push(routeConstant.LINUX_NETWORK, data);
     }
     else {
-    let data = { windowsNetwork: true, editData: true, clientInfo: props.location.state.clientInfo, targetInfo: props.location.state.targetInfo }
+    let data = { 
+      windowsNetwork: true, 
+      editData: true,
+      clientInfo: props.location.state.clientInfo,
+      targetInfo: props.location.state.targetInfo,
+      editLinuxData: props.location.state.editLinuxData ? props.location.state.editLinuxData : false,
+      LinuxNetwork: props.location.state && props.location.state.LinuxNetwork ? props.location.state.LinuxNetwork : false,
+    }
     setRaStepper(client, stepper.Task.name, stepper.Task.value, data);
     history.push(routeConstant.TASK_DETAILS, data);
     }
   }catch {
-    let data = { windowsNetwork: true, editData: true, clientInfo: props.location.state.clientInfo, targetInfo: props.location.state.targetInfo }
+    let data = { windowsNetwork: true, editData: true, clientInfo: props.location.state.clientInfo, targetInfo: props.location.state.targetInfo, editLinuxData: props.location.state.editLinuxData ? props.location.state.editLinuxData : false,
+      LinuxNetwork: props.location.state && props.location.state.LinuxNetwork ? props.location.state.LinuxNetwork : false, }
     setRaStepper(client, stepper.Task.name, stepper.Task.value, data);
     history.push(routeConstant.TASK_DETAILS, data);
   }
