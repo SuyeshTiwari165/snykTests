04933db347e06b03718c38011d1ee87ba31561f8
Suyesh Tiwari <suyeshtiwari123@gmail.com>
2021-12-21 16:49:18 +0530

    Remove Client id filter from getScanConfig Query field

--- src/graphql/queries/ScanConfig.ts
+++ src/graphql/queries/ScanConfig.ts
@@ -1,18 +1,18 @@
 import { gql } from "@apollo/client";
 
 export const GET_SCAN_CONFIG = gql`
-query getScanConfigurationdata(){
-  getScanConfigurationdata(){
-    edges{
-      node{
-        id
-        vatScanConfigId
-        scanConfigName
-        creationTime
-        familyCount
-        nvtCount
+  query getScanConfigurationdata {
+    getScanConfigurationdata {
+      edges {
+        node {
+          id
+          vatScanConfigId
+          scanConfigName
+          creationTime
+          familyCount
+          nvtCount
+        }
       }
     }
   }
-}
 `;
